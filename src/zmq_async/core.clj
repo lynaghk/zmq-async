21f0b82cce92185035eb85e92c659aafd0f6857c
Kevin J. Lynagh <kevin@keminglabs.com>
2013-07-14 11:58:06 -0700

    Add req/rep sockets.

--- src/zmq_async/core.clj
+++ src/zmq_async/core.clj
@@ -1,13 +1,66 @@
 (ns zmq-async.core
+  (:require [clojure.core.async :refer [chan
+                                        go <! >!
+                                        thread <!! >!!]])
   (:import (org.zeromq ZMQ ZMQ$Socket)))
 
 (def context
   (ZMQ/context 1))
 
-(defn socket
-  [socket-type]
-  (.socket context (case socket-type
-                     :pull ZMQ/PULL
-                     :req ZMQ/REQ
-                     :rep ZMQ/REP
-                     :router ZMQ/ROUTER)))
+(def BLOCK 0)
+
+(defn request-socket
+  "Channels supporting the REQ socket of a ZeroMQ REQ/REP pair.
+A message must be sent before one can be recieved (in that order).
+Returns two bufferless channels [in, out]."
+  [addr]
+  (let [in (chan) out (chan)]
+    (future
+      (with-open [sock (.socket context ZMQ/REQ)]
+        (.connect sock addr)
+        ;;TODO: do I need to quit on InterruptedExceptions?
+        (while true
+          (let [msg (<!! out)]
+            (if-not (string? msg)
+              (println "String messages only for now, kthx.")
+              (.send sock msg)))
+          (>!! in (String. (.recv sock BLOCK))))))
+    [in out]))
+
+(defn reply-socket
+  "Channels supporting the REP socket of a ZeroMQ REQ/REP pair.
+A message must be received before one can be sent (in that order).
+Returns two bufferless channels [in, out]."
+  [addr]
+  (let [in (chan) out (chan)]
+    (future
+      (with-open [sock (.socket context ZMQ/REP)]
+        (.bind sock addr)
+        ;;TODO: do I need to quit on InterruptedExceptions?
+        (while true
+          (>!! in (String. (.recv sock BLOCK)))
+          (let [msg (<!! out)]
+            (if-not (string? msg)
+              (println "String messages only for now, kthx.")
+              (.send sock msg))))))
+    [in out]))
+
+
+(comment
+  (let [addr "ipc://grr.ipc"
+        [client-in client-out] (request-socket addr)
+        [server-in server-out] (reply-socket addr)]
+
+    
+    (go ;;client
+      (dotimes [i 5]
+        (>! client-out "hi")
+        (println "server says: " (<! client-in))))
+    
+    (go ;;server
+      (dotimes [i 5]
+        (println "client says:" (<! server-in))
+        (>! server-out (str i)))))
+
+
+  )
---
+++ test/zmq_async/t_core.clj
@@ -0,0 +1,27 @@
+(ns zmq-async.t-core
+  (:require [zmq-async.core :refer [request-socket reply-socket]]
+            [clojure.core.async :refer [go close! >!! <!!]]
+            [midje.sweet :refer :all]))
+
+(def addr "ipc://test_socket.ipc")
+
+(let [[client-in client-out] (request-socket addr)
+      [server-in server-out] (reply-socket addr)
+      n 5]
+
+  (fact "REQ/REP ping pong"
+        (let [server (go
+                       (dotimes [_ n]
+                         (assert (= "ping" (<! server-in)))
+                         (>! server-out "pong"))
+                       :success)
+
+              client (future
+                       (dotimes [_ n]
+                         (>!! client-out "ping")
+                         (assert (= "pong" (<!! client-in))))
+                       :success)]
+
+          (deref client 500 :fail) => :success
+          (close! server)
+          (<!! server) => :success)))
